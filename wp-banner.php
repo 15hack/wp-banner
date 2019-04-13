6beaed6023e81310a677b83fa6cd7eccda235133
15hack <15hack@riseup.net>
2019-04-13 14:07:49 +0200

    sin punto

--- wp-banner.php
+++ wp-banner.php
@@ -49,7 +49,7 @@ function banner_div(){
 	echo '
 <div id="banner_15hack">
 Necesitamos <a href="https://15hack.tomalaplaza.net/financiacion-2019/">donaciones</a>
-para mantener los servicios.
+para mantener los servicios
 </div>
   ';
 }
